70c7a5c5c9c56095f5ce598e8d9b92ce5cdd7af3
Vis7044 <singhvishal7044@gmail.com>
2026-03-17 00:21:42 +0530

    Implement database connection and migration, update Docker configuration for PostgreSQL

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,14 +1,29 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"github.com/unilly-api/db"
+	"github.com/unilly-api/models"
+	"github.com/gin-contrib/cors"
 	"github.com/unilly-api/routes"
 )
 
 func main() {
-	r := gin.Default()
-
+    r := gin.Default()
+	database, err := db.NewDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	database.AutoMigrate(&models.User{})
+	fmt.Println("Database connection established:", database)
+    r.GET("/", func(c *gin.Context) {
+        
+        c.String(200, "Hello, World!")
+    })
+	
 	r.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
---
+++ db/db.go
@@ -0,0 +1,35 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func NewDB() (*gorm.DB, error) {
+
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		host, user, password, dbname, port,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return nil, err
+	}
+
+	sqlDB, _ := db.DB()
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+
+	return db, nil
+}
